ec84fe941aec8e37b3844c92df7ac1b790c392ec
KORAKUTUI <155162850+KORAKUTUI@users.noreply.github.com>
2024-01-24 17:53:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+var score=0;
+function decrease(){
+    score=score-10;
+    document.getElementById("box").textContent=score;
+    
+    
+}
+function increase(){
+    score=score+10;
+    document.getElementById("box").textContent=score;}
+
+
+function hide(){
+    document.getElementById("hideme").style.display="none";
+    
+}
+
+[15:24, 24/01/2024] Shaik Mubeen: ;
+    
+}
+.img3{
+    border-radius: 10px 40px;
+}
+.img4{
+    border-radius: 60px 70px 140px;
+}
+.img5{border-radius: 30% 30%/80% 80%;
+}
+.img1:hover{
+    border-radius: 30% 30%/80% 80%;
+    box-shadow: 15px 20px 20px black;
+    margin-bottom: 10px;}
+.img2:hover{
+    border-radius: 50px 70px 30px;
+    box-shadow: 15px 20px 20px blue;
+    margin-bottom: 10px; }
+.img3:hover{
+    border-radius: 80px 40px;
+    box-shadow: 35px 20px 20px red;
+    margin-bottom: 10px; }
+.img4:hover{
+    border-radius: 60px;
+    box-shadow: 15px 20px 20px grey;
+    margin-bottom: 10px; }
+.img5:hover{
+    border-radius: 60px 50%;
+    box-shadow: 15px 20px 20px aqua;
+    margin-bottom: 60px; }
+
+
+
+.img10{
+    width: 55px;
+    height: 5px;
+    
+}
+
+.img11{
+    width: 200px;
+    height: 200px;
+    
+}
+.img12{
+    width: 200px;
+    height: 200px;
+}
+#d1{
+    width:80px;
+    height:60px;
+    background-color:cyan;
+    border:5px double black; 
+}
+#hideme{
+    font-size:50 px;
+    background-color:teal;
+    border:5px double yellow;
+}
+body{
+    background-color:;
+    
